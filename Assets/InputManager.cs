69f04065b3c1b230af36712bb1c04cb82333be2a
arnaudjopart <arnaud.jopart@gmail.com>
2016-06-27 00:32:40 +0200

    Initial Commit

---
+++ Assets/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+
+    #region Public And Protected Members
+
+    public float m_speed;
+    public Player m_playerOne;
+    public GameObject m_playerTwo;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        ManageInputPlayerOne();
+
+    }
+
+    void ManageInputPlayerOne()
+    {
+        float HPad = Input.GetAxis("HorizontalPad");
+        float VPad = Input.GetAxis("VerticalPad");
+
+        print(VPad);
+
+        playerOneMoveInput = new Vector2(HPad, VPad);
+
+        m_playerOne.ManageMoveInput(playerOneMoveInput);
+               
+    }
+   
+
+    #region Private Members
+
+    private Rigidbody m_rb;
+    private Transform m_transform;
+
+    private Vector2 playerOneMoveInput;
+
+    #endregion  
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    #region Public and Protected Members
+
+    public GameObject m_bulletPrefab;
+    public Transform cannon;
+    public InputManager m_inputManager;
+    public float m_speed;
+
+    #endregion
+
+
+    // Use this for initialization
+    void Start () {
+        m_rb = GetComponent<Rigidbody>();
+        m_transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+        if( Input.GetKeyDown( KeyCode.Space ) )
+        {
+            Shoot();
+        }
+	}
+
+    public void ManageMoveInput(Vector2 _vector)
+    {
+        Vector2 moveInput = _vector;
+
+        m_rb.velocity = m_transform.forward * m_speed*moveInput.y;
+
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, moveInput.x*(2+Mathf.Abs(moveInput.y)*3), 0));
+        m_transform.rotation *= rotation; 
+
+    }
+
+    private void Shoot()
+    {
+        Debug.Log( "Shoot" );
+        GameObject bullet = Instantiate(m_bulletPrefab,cannon.position,m_transform.rotation) as GameObject;
+
+    }
+    private Rigidbody m_rb;
+    private Transform m_transform;
+}
