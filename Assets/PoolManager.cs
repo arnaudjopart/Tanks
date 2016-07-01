8ccb420bbe4ef92380c1829b3760ef9522684eae
arnaudjopart <arnaud.jopart@gmail.com>
2016-07-02 00:46:58 +0200

    Pool system for projectiles

---
+++ Assets/PoolManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoolManager : MonoBehaviour {
+
+    #region Public And Protected Members
+    public int m_nbOfInstancies;
+    public Transform m_prefab;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        m_transform = GetComponent<Transform>();
+        FillPool();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Transform GetFirstActiveFalse()
+    {
+        foreach(Transform obj in m_listOfInstancies)
+        {
+            if (!obj.gameObject.activeSelf)
+            {
+                obj.gameObject.SetActive(true);
+                return obj;
+            }
+        }
+
+        Transform newInstance = CreateInstance();
+        m_listOfInstancies.Add(newInstance);
+        return newInstance;
+
+    }
+    #region Utils
+
+    private void FillPool()
+    {
+        for (int i = 0; i < m_nbOfInstancies; i++)
+        {
+            Transform instance = CreateInstance();
+
+            m_listOfInstancies.Add(instance);
+
+        }
+    }
+    private Transform CreateInstance()
+    {
+        Transform instance = Instantiate(m_prefab, m_transform.position, Quaternion.identity) as Transform;
+        instance.SetParent(m_transform, false);
+        instance.gameObject.SetActive(false);
+        
+
+        return instance;
+        
+    }
+
+    
+    #endregion
+    #region Private Members
+    private Transform m_transform;
+    private List<Transform> m_listOfInstancies = new List<Transform>();
+    #endregion
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    #region Public And Protected Members
+    public float m_liveTime;
+    #endregion
+    // Use this for initialization
+    void Start () {
+        m_transform = GetComponent<Transform>();
+        startLiveTimer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_transform.Translate(m_transform.InverseTransformVector(m_transform.forward)*10*Time.deltaTime);
+        if (Time.time > startLiveTimer + m_liveTime)
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+
+    #region Private Memebers
+    private Transform m_transform;
+    private float startLiveTimer;
+    #endregion
+}
